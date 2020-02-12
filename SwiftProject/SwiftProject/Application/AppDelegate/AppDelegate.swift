3417bc4605581fc86d87bd4d118545fb7cfbe9c0
ryan at home <292349635@qq.com>
2020-02-12 11:06:56 +0800

    2

--- SwiftProject/SwiftProject/Application/AppDelegate/AppDelegate.swift
+++ SwiftProject/SwiftProject/Application/AppDelegate/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         ProfileAssembly.profileRegister()
         BPListAssembly.profileRegister()
+        
         print("DidFinishLaunch")
         
         
