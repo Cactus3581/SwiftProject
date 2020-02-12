596cfb1b80690b9d23362df566d9b53f3c97846c
xiaruzhen <xiaruzhen@bytedance.com>
2020-02-12 12:35:39 +0800

    lark1
    Change-Id: Ic599cca1e0e04ea1ee73e41d01d568c399740cf8

--- SwiftProject/SwiftProject/Application/AppDelegate/AppDelegate.swift
+++ SwiftProject/SwiftProject/Application/AppDelegate/AppDelegate.swift
@@ -33,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // lark
     //lark
     //lark
+    //lark
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         print("将进入前台")
