ce2add59521b237c6c76cbeb03ddaf28ea47d169
xiaruzhen <xiaruzhen@bytedance.com>
2020-02-12 21:47:49 +0800

    ----
    Change-Id: Ifb0a58b8e02981111c01ab924c79bbf165b1b277

--- SwiftProject/SwiftProject/Application/AppDelegate/SceneDelegate.swift
+++ SwiftProject/SwiftProject/Application/AppDelegate/SceneDelegate.swift
@@ -115,13 +115,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
         print("已变成活跃")
-        print("已变成活跃")
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-        print("将取消活跃")
         print("将取消活跃")
     }
 
@@ -129,7 +127,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        print("已进入后台")
         print("已进入后台")
         //1
         //2
