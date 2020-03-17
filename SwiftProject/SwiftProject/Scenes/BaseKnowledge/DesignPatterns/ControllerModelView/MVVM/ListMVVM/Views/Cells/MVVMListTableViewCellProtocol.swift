31e441cf181ff06cce16aaf5ba92ab811bcaeab7
xiaruzhen <xiaruzhen@bytedance.com>
2020-03-17 18:04:02 +0800

    session
    Change-Id: I2e8f189e485157fa5db37729a00f1c4729e5adc9

---
+++ SwiftProject/SwiftProject/Scenes/BaseKnowledge/DesignPatterns/ControllerModelView/MVVM/ListMVVM/Views/Cells/MVVMListTableViewCellProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  MVVMListBaseTableViewCell.swift
+//  SwiftProject
+//
+//  Created by 夏汝震 on 2020/3/11.
+//  Copyright © 2020 cactus. All rights reserved.
+//
+
+import UIKit
+
+protocol MVVMListTableViewCellProtocol {
+    var cellViewModel: MVVMListCellViewModelProtocol? { set get }
+    static var identifier: String { get }
+}
+
+extension MVVMListTableViewCellProtocol {
+    var cellViewModel: MVVMListCellViewModelProtocol? {set{} get{return nil} }
+}
